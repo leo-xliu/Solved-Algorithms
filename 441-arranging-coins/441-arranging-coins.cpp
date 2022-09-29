32bfe116abd2fafed77ffe48c76cc6d9ec7bcce3
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-09-29 13:59:03 -0700

    Time: 3 ms (84.81%), Space: 5.9 MB (69.32%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int arrangeCoins(int n) {;
-        int i = 1;
-        while (n >= 0){
-            n -= i;
-            i++;
-            if (n < i){
-                break;
+        int l(1), h(n);
+        int res = 0;
+        while (l <= h){
+            int mid = (h-l)/2 + l;
+            if (mid <= (double) n*2/(mid+1)){
+                res = max(res, mid);
+                l = mid+1;
             }
+            else h = mid-1;
         }
-        return --i;
+        return res;
     }
 };
 
-//have an array that for the ith row tells you how many coins you can have total
 
