e0f547a6241386e54ab6604b586f7edd63e0c525
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-10-02 21:58:33 -0700

    Time: 124 ms (18.38%), Space: 14.6 MB (32.78%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,25 +9,32 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode*> interaddr; 
-        ListNode* l1 = headA;
-        ListNode* l2 = headB;
-        while (l1 || l2){
-            if (l1){
-                if (interaddr.find(l1) != interaddr.end()) return l1;
-                else {
-                    interaddr.insert(l1);
-                    l1 = l1->next;
-                }
-            }
-            if (l2){
-                if (interaddr.find(l2) != interaddr.end()) return l2;
-                else {
-                    interaddr.insert(l2);
-                    l2 = l2->next;
-                }
-            }
+        ListNode* node = headA;
+        while (node){
+            node->val *= -1;
+            node = node->next;
         }
-        return nullptr;
+        node = headB;
+        while (node){
+            if (node->val < 0){
+                break;
+            }
+            node = node->next;
+        }
+        ListNode* revert = headA;
+            while (revert){
+                revert->val *= -1;
+                revert = revert->next;
+            }
+        return node;
     }
 };
+
+//to do this with constant space complexity, simply visit the first linked list and set each prev nodes next node to itself
+//this way if we iterate through the second list, we will always land on the node where the next value is itself
+//that node will be the intersection
+//if there is no intersection the list will end in nullptr
+//this breaks the rule of retaining original structure
+//new idea: make each val negative in first list since val must be positive
+//when iterating second list if we find neg value that is the intersection
+//before returning make all values positive again
