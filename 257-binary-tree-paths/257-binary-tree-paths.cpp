01e7e8c50a67d8534a67da0b5d9c84471db6090e
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-10-06 14:06:02 -0700

    Time: 3 ms (90.21%), Space: 13 MB (77.33%) - LeetHub

---
+++ 257-binary-tree-paths/257-binary-tree-paths.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> res;
+        pathHelper(root,to_string(root->val), res);
+        return res;
+    }
+    void pathHelper(TreeNode* cur, string curpath, vector<string>& res){
+        if (!cur->left && !cur->right){
+            res.push_back(curpath);
+            return;
+        }
+        if (cur->left) pathHelper(cur->left, curpath+"->"+to_string(cur->left->val), res);
+        if (cur->right) pathHelper(cur->right, curpath+"->"+to_string(cur->right->val), res);
+    }
+};
+
+
+//tell leo tobin went on lunch break
