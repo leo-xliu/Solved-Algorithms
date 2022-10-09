0c3b032a04fa331e471af81a4c73a69fcd62adea
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-10-09 01:25:43 -0700

    Time: 796 ms (20.00%), Space: 76.8 MB (31.01%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<double, vector<int>>, vector<pair<double, vector<int>>>, greater<pair<double, vector<int>>>> pq;
+        priority_queue<pair<double, vector<int>>> pq;
         for(int i = 0; i < points.size(); i++){
             int x = points[i][0];
             int y = points[i][1];
             double sqr = (double) sqrt(x*x+y*y);
             pq.push({sqr, points[i]});
+            if (pq.size() > k) pq.pop();
         }
         vector<vector<int>> res;
-        for (int i = 0; i < k; i++){
+        while (!pq.empty()){
             pair<double, vector<int>> temp = pq.top();
             pq.pop();
             res.push_back(temp.second);
@@ -17,3 +18,7 @@ public:
         return res;
     }
 };
+
+//to make solution nlogk, use a max heap of size k 
+//this stores the k smallest values since we always remove max
+// min heap with size k stores k largest values since we always remove min
