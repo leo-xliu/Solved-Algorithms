771704f67345e8a6eb8567de3a6d5d0543e8f51a
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-10-10 22:37:25 -0700

    Time: 4 ms (96.66%), Space: 9.8 MB (39.18%) - LeetHub

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     vector<int> maxSubsequence(vector<int>& nums, int k) {
-        priority_queue<int> pq;
-        for (int i = 0; i < nums.size(); i++){
-            pq.push(nums[i]);
-        }
-        unordered_map<int, int> map;
-        for (int i = 0; i < k; i++){
-            int temp = pq.top();
-            pq.pop();
-            map[temp]++;
-        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> pq;
         vector<int> res;
         for (int i = 0; i < nums.size(); i++){
-            if (map.find(nums[i]) != map.end()){
-                res.push_back(nums[i]);
-                if (map[nums[i]]-- == 1) map.erase(nums[i]);
+            pq.push({nums[i], i});
+            if (pq.size() > k){
+                pq.pop();
             }
+        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> resort;
+        while (!pq.empty()){
+            pair<int, int> temp = pq.top();
+            pq.pop();
+            resort.push({temp.second, temp.first});
+        }
+        while (!resort.empty()){
+            int temp = resort.top().second;
+            resort.pop();
+            res.push_back(temp);
         }
         return res;
     }
 };
+
+//use a kminheap
