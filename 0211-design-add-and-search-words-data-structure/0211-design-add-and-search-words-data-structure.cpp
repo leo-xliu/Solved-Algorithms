fb752209701a183acf4ddb1819a3d3c696a14e20
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-10-11 22:54:34 -0700

    Time: 1798 ms (59.38%), Space: 558.5 MB (49.70%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -1,60 +1,53 @@
-class TrieNode {
-public:
+struct TrieNode {
     TrieNode* children[26];
     bool isWord;
-    
     TrieNode() {
         for (int i = 0; i < 26; i++) {
-            children[i] = NULL;
+            children[i] = nullptr;
         }
         isWord = false;
     }
 };
 
 class WordDictionary {
-public:
-    WordDictionary() {
-        root = new TrieNode();
-    }
-    
-    void addWord(string word) {
-        TrieNode* node = root;
-        int curr = 0;
-        
-        for (int i = 0; i < word.size(); i++) {
-            curr = word[i] - 'a';
-            if (node->children[curr] == NULL) {
-                node->children[curr] = new TrieNode();
-            }
-            node = node->children[curr];
-        }
-        
-        node->isWord = true;
-    }
-    
-    bool search(string word) {
-        TrieNode* node = root;
-        return searchInNode(word, 0, node);
-    }
 private:
-    TrieNode* root;
-    
-    bool searchInNode(string& word, int i, TrieNode* node) {
-        if (node == NULL) {
+    TrieNode* head;
+    bool searchHelper(string& word, int i, TrieNode* node) {
+        if (!node) {
             return false;
         }
         if (i == word.size()) {
             return node->isWord;
         }
         if (word[i] != '.') {
-            return searchInNode(word, i + 1, node->children[word[i] - 'a']);
+            return searchHelper(word, i + 1, node->children[word[i] - 'a']);
         }
         for (int j = 0; j < 26; j++) {
-            if (searchInNode(word, i + 1, node->children[j])) {
+            if (searchHelper(word, i + 1, node->children[j])) {
                 return true;
             }
         }
         return false;
+    }
+public:
+    WordDictionary() {
+        head = new TrieNode();
+    }
+    
+    void addWord(string word) {
+        TrieNode* node = head;
+        for (int i = 0; i < word.size(); i++) {
+            int curr = word[i] - 'a';
+            if (!node->children[curr]) {
+                node->children[curr] = new TrieNode();
+            }
+            node = node->children[curr];
+        }
+        node->isWord = true;
+    }
+    
+    bool search(string word) {
+        return searchHelper(word, 0, head);
     }
 };
 
