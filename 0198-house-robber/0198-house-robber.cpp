cdd1fb18187234cc9550b2a17778a961858a7fd0
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-10-13 23:38:22 -0700

    Time: 0 ms (100.00%), Space: 7.6 MB (77.58%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if (nums.size() == 1) return nums[0];
-        if (nums.size() == 2) return (max(nums[0], nums[1]));
-        int prevsecond = nums[0];
-        int prevfirst = nums[1];
-        int cur = nums[2]+prevsecond;
-        for (int i = 3; i < nums.size(); i++){
-            int temp = nums[i] + max(prevsecond, prevfirst);
-            prevsecond = prevfirst;
-            prevfirst = cur;
-            cur = temp;
+        int prev = 0;
+        int cur = 0;
+        int next = 0;
+        for (int i = 0; i < nums.size(); i++){
+            next = max(prev+nums[i], cur);
+            prev = cur;
+            cur = next;
         }
-        return max(cur, prevfirst);
+        return cur;
     }
 };
