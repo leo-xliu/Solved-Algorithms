4557aba64f8ffa1d39bd2d375b571eb02236e889
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-10-14 23:01:42 -0700

    Time: 158 ms (20.13%), Space: 55.4 MB (93.16%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -3,21 +3,18 @@ public:
     int longestOnes(vector<int>& nums, int k) {
         int l(0), r(0);
         int res = 0;
-        int curmax = 0;
         int runningk = 0;
         for (; r < nums.size(); r++){
-            curmax++;
             if (nums[r] == 0){
                 runningk++;
             }
             if (runningk > k){
-                curmax--;
                 if (nums[l++] == 0){
                     runningk--;
                 }
             }
             else {
-                res = max(curmax, res);
+                res = max(r-l+1, res);
             }
         }
         return res;
