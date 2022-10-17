aec7f91c2ec0aeabbea8cfbe2a77883288367f29
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-10-17 08:13:25 -0700

    Time: 2 ms (67.82%), Space: 6 MB (98.73%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -2,23 +2,15 @@ class Solution {
 public:
     int numDecodings(string s) {
         if (s[0] == '0') return 0;
-        set<string> code; 
-        for (int i = 1; i < 27; i++){
-            code.insert(to_string(i));
-        }
         int res1 = 1;
         int res2 = 1;
         int res = 1; 
-        char first = s[0];
-
         for (int i = 1; i < s.length(); i++){
             res = 0;
-            string temp = "";
-            temp += s[i];
-            if (code.find(temp) != code.end()) res += res1;
-            temp = first+temp;
-            if (code.find(temp) != code.end()) res += res2;
-            first = s[i];
+            int ones = stoi(s.substr(i, 1));
+            if (ones >= 1 && ones <= 9) res += res1;
+            int tens = stoi(s.substr(i-1, 2));
+            if (tens >= 10 && tens <= 26) res += res2;
             res2 = res1;
             res1 = res;
         }
