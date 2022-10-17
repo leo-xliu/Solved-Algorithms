c5976bf9a0287e53bdb5d08d5e63b950cd897c7a
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-10-17 16:55:08 -0700

    Time: 0 ms (100.00%), Space: 5.9 MB (78.20%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0) return 0;
+        int first = 0;
+        int second = 1;
+        for (int i = 2; i <= n; i++){
+            int third = first+second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
+};
