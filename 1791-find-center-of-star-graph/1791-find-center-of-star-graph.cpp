461ee8ccee05879e98a7e0714679baf0e7573cdc
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-10-17 22:50:26 -0700

    Time: 498 ms (27.63%), Space: 67.1 MB (97.42%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        unordered_set<int> set;
-        for(int i = 0; i < edges.size(); i++){
-            int v1 = edges[i][0];
-            int v2 = edges[i][1];
-            if (set.find(v1) != set.end()) return v1;
-            else set.insert(v1);
-            if (set.find(v2) != set.end()) return v2;
-            else set.insert(v2);
-        }
-        return 0;
+        if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) return edges[0][0];
+        else return edges[0][1];
     }
 };
