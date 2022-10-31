c919978523193f898456064cacd07717cbb07481
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-10-31 11:42:06 -0700

    Time: 67 ms (11.19%), Space: 17.3 MB (5.48%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        //create our prerequisite hashmap 
         vector<unordered_set<int>> courses(numCourses);
         for (int i = 0; i < prerequisites.size(); i++){
             vector<int> pair = prerequisites[i];
@@ -11,25 +12,37 @@ public:
             }
             courses[course].insert(prereq);
         }
-        vector<bool> validCourses(numCourses);
-        int checkedCourses = 0;
-        //remove each edge until none of edges
-        while (checkedCourses != numCourses){
-            queue<int> removePrereq;
-            for (int i = 0; i < numCourses; i++){
-                if (courses[i].empty() && !validCourses[i]){
-                    validCourses[i] = true;
-                    checkedCourses++;
-                    removePrereq.push(i);
-                }
+        
+        //remove each edge until none have prereqs
+        //check if each course is valid
+        for (int i = 0; i < numCourses; i++){
+            if (courses[i].empty()){
+                continue;
             }
-            if (removePrereq.empty()) return false;
-            while (!removePrereq.empty()){
-                int curCourse = removePrereq.front();
-                removePrereq.pop();
-                for (int i = 0; i < numCourses; i++){
-                    courses[i].erase(curCourse);
-                }
+            vector<bool> visited(numCourses);
+            if (!dfs(courses, visited, i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    bool dfs(vector<unordered_set<int>>& courses, vector<bool>& visited, int curCourse){
+        if (courses[curCourse].empty()){
+            return true;
+        }
+        if (visited[curCourse]){
+            return false;
+        }
+        visited[curCourse] = true;
+        for (auto p = courses[curCourse].begin(); p != courses[curCourse].end();){
+            if (dfs(courses, visited, *p)){
+                auto temp = p;
+                p++;
+                courses[curCourse].erase(*temp);
+            }  
+            else {
+                return false;
             }
         }
         return true;
