8286f621ec2d8c899d704d0d69f6d25d88f8c929
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-10-31 23:43:20 -0700

    Time: 75 ms (7.25%), Space: 17.9 MB (5.01%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> res;
+        unordered_set<int> ordered;
+        vector<unordered_set<int>> courses(numCourses);
+        for (int i = 0; i < prerequisites.size(); i++){
+            vector<int> prereq = prerequisites[i];
+            courses[prereq[0]].insert(prereq[1]);
+        }
+        for (int i = 0; i < numCourses; i++){
+            if (ordered.find(i) == ordered.end()){
+                vector<bool> visited(numCourses);
+                if (!dfs(courses, visited, res, ordered, i)){
+                    return {};
+                } 
+            }
+        }
+        return res;
+    }
+    
+    bool dfs(vector<unordered_set<int>>& courses, vector<bool>& visited, vector<int>& res, unordered_set<int>& ordered, int course){
+        if (ordered.find(course) != ordered.end()){
+            return true;
+        }
+        if (visited[course]){
+            return false;
+        }
+        visited[course] = true;
+        for (auto p = courses[course].begin(); p != courses[course].end();){
+            if (dfs(courses, visited, res, ordered, *p)){
+                auto temp = p; 
+                p++;
+                courses[course].erase(*temp);
+            }
+            else {
+                return false;
+            }
+        }
+        if (ordered.find(course) == ordered.end()){
+            res.push_back(course);
+            ordered.insert(course);
+        }
+        return true;
+    }
+};
