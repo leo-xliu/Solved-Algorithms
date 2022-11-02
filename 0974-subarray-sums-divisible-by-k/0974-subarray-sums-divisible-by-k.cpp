7d4ba3f2c8c039789d504215e20ed896a35099ca
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-11-01 18:33:29 -0700

    Time: 135 ms (38.98%), Space: 30.2 MB (87.15%) - LeetHub

---
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        vector<int> remainders(k);
+        int res = 0; 
+        int sum = 0;
+        remainders[0] = 1;
+        for (int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+            int remainder = sum % k;
+            if (remainder < 0){
+                remainder += k;
+            }
+            res += remainders[remainder];
+            remainders[remainder]++;
+        }
+        return res;
+    }
+};
+
+
+//use prefix sum remainders
+//we are trying to match remainder with past remainders
+//if we match, it means there are subarrays that are divisible
+//take into account negative numbers
+//just make them positive since sign doesn't do anything in terms of divisibility
+//
