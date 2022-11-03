b306736c6064a74d93ee85de2a5746d477eefafd
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-11-03 14:21:11 -0700

    Time: 0 ms (100.00%), Space: 6.9 MB (25.89%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) { 
+        if (nums.size() == 0){
+            return {};
+        }
+        vector<string> res;
+        int start = 0; 
+        int end = 0;
+        for (int i = 1; i < nums.size(); i++){
+            if (nums[end]+1 != nums[i]){
+                string range = to_string(nums[start]);
+                if (start != end){
+                    range += "->"+to_string(nums[end]);
+                }
+                res.push_back(range);
+                start = i;
+                end = i;
+            }
+            else {
+                end++;
+            }
+        }
+        string range = to_string(nums[start]);
+        if (start != end){
+            range += "->"+to_string(nums[end]);
+        }
+        res.push_back(range);
+        return res;
+    }
+};
