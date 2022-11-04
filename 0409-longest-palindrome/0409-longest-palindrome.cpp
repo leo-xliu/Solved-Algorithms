61575b4b36e2d093ecfae3375177fc7491369244
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-11-04 15:25:09 -0700

    Time: 8 ms (38.92%), Space: 7.5 MB (5.10%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char, int> map;
+        int count = 0;
+        for (char ch : s){
+            if (++map[ch] == 2){
+                count += 2;
+                map.erase(ch);
+            }
+        }
+        if (!map.empty()){
+            count++;
+        }
+        return count;
+    }
+};
