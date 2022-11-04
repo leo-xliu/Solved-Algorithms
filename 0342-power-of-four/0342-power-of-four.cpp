e376b3633a2143b539d3900c867b7248f576c9ee
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-11-04 13:35:54 -0700

    Time: 3 ms (63.48%), Space: 5.9 MB (71.96%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if (n > 0 && !(n & (n-1))){
-            return(n & 1431655765);
-        }
-        return false;
+        return (n > 0 && !(n & (n-1)) && !((n-1) % 3));
     }
 };
