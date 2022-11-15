65045e4c322aa4870a74fc36c2bd29c872512e0a
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-11-15 12:59:53 -0800

    Time: 2 ms (78.68%), Space: 6.4 MB (97.42%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        if (s.length() > 12){
+            return {};
+        }
+        vector<string> res;
+        dfs(res, s, 0, 1);
+        return res;
+    }
+
+    void dfs(vector<string>& res, string& s, int start, int period){
+        if (period == 4){
+            if (stoi(s.substr(start, s.length()-start)) <= 255 && (s.length()-start == 1 || s[start] != '0')){
+                res.push_back(s);
+            }
+            return;
+        }
+        for (int i = 1; i <=3; i++){
+            if (start+i >= s.length() || i == 3 && stoi(s.substr(start, i)) > 255 || i != 1 && s[start] == '0'){
+                break;
+            }
+            s.insert(start+i, ".");
+            dfs(res, s, start+i+1, period+1);
+            s.erase(start+i, 1);
+        }
+    }
+};
+
+
+//use string insert to add periods
+//we are paritioning by using 3 periods
