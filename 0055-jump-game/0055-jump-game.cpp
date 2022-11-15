f67f915954c6b722c4d1462e15bf7eb70a58bcd4
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-11-15 15:59:49 -0800

    Time: 63 ms (97.39%), Space: 48.4 MB (78.88%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        if (nums[0] == 0 && nums.size() != 1){
-            return false;
-        }
-        for (int i = nums.size()-2; i >= 0; i--){
-            if (nums[i] == 0){
-                int jumpsNeeded = 2;
-                bool jumpable = false;
-                for (int j = i-1; j >= 0; j--){
-                    if (nums[j] >= jumpsNeeded){
-                        i = j;
-                        jumpable = true;
-                        break;
-                    }
-                    jumpsNeeded++;
-                }
-                if (!jumpable){
-                    return false;
-                }
+        int n = nums.size();
+        int maxReachable = 0;
+        for (int i = 0; i < n; i++){
+            if (maxReachable < i){
+                return false;
+            }
+            maxReachable = max(maxReachable, i + nums[i]);
+            if (maxReachable >= n-1){
+                break;
             }
         }
         return true;
