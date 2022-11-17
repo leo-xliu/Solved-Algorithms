6aed37a0fe223e8c29c7c0e02ff08ba9b2c3c46f
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-11-16 22:56:32 -0800

    Time: 0 ms (100.00%), Space: 6.5 MB (61.22%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -3,25 +3,19 @@ public:
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> res;
         vector<int> curSum;
-        unordered_set<int> curSet;
-        dfs(res, curSum, curSet, k, 0, n, 1);
+        dfs(res, curSum, k, 0, n, 1);
         return res;
     }
     
-    void dfs(vector<vector<int>>& res, vector<int>& curSum, unordered_set<int>& curSet, int k, int sum, int n, int start){
+    void dfs(vector<vector<int>>& res, vector<int>& curSum, int k, int sum, int n, int start){
         if (k == 0 && sum == n){
             res.push_back(curSum);
             return;
         }
         for (int i = start; i <= 9; i++){
-            if (curSet.find(i) != curSet.end()){
-                continue;
-            }
-            curSet.insert(i);
             curSum.push_back(i);
-            dfs(res, curSum, curSet, k-1, sum+i, n, i+1);
+            dfs(res, curSum, k-1, sum+i, n, i+1);
             curSum.pop_back();
-            curSet.erase(i);
         }
     }
 };
