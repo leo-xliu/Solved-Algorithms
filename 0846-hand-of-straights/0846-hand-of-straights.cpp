544e35b00ea9cd8d1bedf28063f110184cd37693
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-11-17 22:00:36 -0800

    Time: 80 ms (95.18%), Space: 28.1 MB (75.16%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        unordered_map<int, int> count;
-        priority_queue<int, vector<int>, greater<int>> minheap;
+        if (hand.size() % groupSize != 0){
+            return false;
+        }
+        map<int, int> count;
         for (int i = 0; i < hand.size(); i++){
-            if (count.find(hand[i]) == count.end()){
-                minheap.push(hand[i]);
-            }
             count[hand[i]]++;
         }
-        while (!minheap.empty()){
-            int min = minheap.top();
+        while (!count.empty()){
+            int min = count.begin()->first;
             for (int i = min; i < min+groupSize; i++){
                 if (count.find(i) == count.end()){
                     return false;
                 }
                 if (--count[i] == 0){
                     count.erase(i);
-                    minheap.pop();
                 }
             }
         }
