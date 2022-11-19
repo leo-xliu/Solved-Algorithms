f06cc78aff384647cec03d09c93191a6dedb9b13
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-11-19 09:10:59 -0800

    Time: 1312 ms (15.68%), Space: 163.6 MB (20.46%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
-        bool trip1(false), trip2(false), trip3(false);
-        int tar1(target[0]), tar2(target[1]), tar3(target[2]);
-        int t1, t2, t3;
-        for (int i = 0; i < triplets.size(); i++){
-            t1 = triplets[i][0];
-            t2 = triplets[i][1];
-            t3 = triplets[i][2];
-            if (tar1 == t1 && t2 <= tar2 && t3 <= tar3){
-                trip1 = true;
-            }
-            if (tar2 == t2 && t1 <= tar1 && t3 <= tar3){
-                trip2 = true;
-            }
-            if (tar3 == t3 && t2 <= tar2 && t1 <= tar1){
-                trip3 = true;
+        int tar0(target[0]), tar1(target[1]), tar2(target[2]);
+        vector<int> res(3);
+        for (auto trip : triplets){
+            if (trip[0] <= tar0 && trip[1] <= tar1 && trip[2] <= tar2){
+                res = {max(res[0], trip[0]), max(res[1], trip[1]), max(res[2], trip[2])};
             }
         }
-        return (trip1 && trip2 && trip3);
+        return (res == target);
     }
 };
