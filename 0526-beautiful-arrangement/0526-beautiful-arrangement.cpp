bbe2cfa3def9f0d9a30aab18ae75f862b8061519
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-11-21 23:52:02 -0800

    Time: 108 ms (69.85%), Space: 6 MB (56.02%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -2,22 +2,21 @@ class Solution {
 public:
     int countArrangement(int n) {
         int res = 0;
-        unordered_set<int> set;
-        dfs(set, res, 1, n);
+        vector<bool> check(n+1, false);
+        dfs(check, res, 1, n);
         return res;
     }
     
-    void dfs(unordered_set<int>& set, int& res, int start, int n){
+    void dfs(vector<bool>& check, int& res, int start, int n){
         if (start == n+1){
             res++;
             return;
         }
         for (int i = 1; i <= n; i++){
-            if (set.find(i) != set.end()) continue;
-            if (i % start == 0 || start % i == 0){
-                set.insert(i);
-                dfs(set, res, start+1, n);
-                set.erase(i);
+            if (!check[i] && (i % start == 0 || start % i == 0)){
+                check[i] = true;
+                dfs(check, res, start+1, n);
+                check[i] = false;
             }
         }
     }
