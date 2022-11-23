eae888690b5aa5e355e8d286003760e7f221fc4b
Leo Liu <85960440+leo-xliu@users.noreply.github.com>
2022-11-22 20:11:26 -0800

    Time: 12 ms (15.56%), Space: 6.9 MB (26.44%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        string res;
+        unordered_map<char, int> map;
+        unordered_set<char> inRes;
+        for (char ch : s){
+            map[ch]++;
+        }
+        for (char ch: s){
+            if (map.find(ch) == map.end()){
+                continue;
+            }
+            if (inRes.find(ch) == inRes.end()){
+                while (res.length() != 0 && res.back() > ch && map.find(res.back()) != map.end()){
+                    inRes.erase(res.back());
+                    res.pop_back();
+                }
+                res.push_back(ch);
+                inRes.insert(ch);
+            }
+            if (--map[ch] == 0){
+                map.erase(ch);
+            }
+        }
+        return res;
+    }
+};
